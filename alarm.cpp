abe7f16cbdbd3f3c8489a992119eac0d05f62680
Joel Van Auken <jvanauke@purdue.edu>
2016-02-28 16:50:41 -0500

    Make Alarm Clock

--- alarm.cpp
+++ alarm.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
+#include <ctime>
 
+#include "alarm.h"
+
+using namespace std;
 
 int main() {
+
+	time_t timeO = time(NULL);
+	tm* t = localtime(&timeO);
+	
+	cout<< "seconds " << t->tm_sec << endl;
+	cout << "minutes " << t->tm_min << endl;
+	cout << "hours " << t->tm_hour << endl;
+	cout << "day " << t->tm_mday << endl;
+	cout << "month " << t->tm_mon << endl;
+	cout << "year " << t->tm_year << endl;
+	cout << "weekday " << t->tm_wday << endl;
+	cout << "day of year " << t->tm_yday << endl;
+	cout << "daylight savings " << t->tm_isdst << endl;
+
 	return 0;
 }
---
+++ alarm.h
@@ -0,0 +1,19 @@
+#ifndef __ALARM_H__
+#define __ALARM_H__
+
+class Alarm{
+	public:
+		Alarm() {};
+		
+	private:
+
+};
+
+class Clock{
+	public:
+		Clock(){};
+	private:
+
+};
+
+#endif
